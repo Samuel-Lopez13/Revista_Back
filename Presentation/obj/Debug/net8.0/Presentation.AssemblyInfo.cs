cf2a8b023e1b04fb5a8bd2f7da091d301f09519c
Samuel-Lopez13 <lopez180219@gmail.com>
2024-03-22 02:01:36 -0600

    modificaciones

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d78d050ddd65e20bc82a01f92c8ead6c8accda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ef1036b9509f8fb52b12f3480f8918c344de13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
